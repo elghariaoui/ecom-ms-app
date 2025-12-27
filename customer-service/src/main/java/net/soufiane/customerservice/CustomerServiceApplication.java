f992058a28a842a321c7c47c4e67362487f782a8
elghariaoui <selghariaoui@gmail.com>
2025-12-27 13:22:17 +0100

    maj

--- customer-service/src/main/java/net/soufiane/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/net/soufiane/customerservice/CustomerServiceApplication.java
@@ -1,14 +1,17 @@
 package net.soufiane.customerservice;
 
 import net.soufiane.customerservice.Repository.CustomerRepository;
+import net.soufiane.customerservice.config.CustomerConfigParams;
 import net.soufiane.customerservice.entities.Customer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
--- customer-service/src/main/java/net/soufiane/customerservice/config/ConfigTestRestController.java
+++ customer-service/src/main/java/net/soufiane/customerservice/config/ConfigTestRestController.java
@@ -1,4 +1,34 @@
 package net.soufiane.customerservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/config-test")
+@RefreshScope
 public class ConfigTestRestController {
+
+    @Value("${global.params.p1}")
+    private String a;
+    @Value("${global.params.p2}")
+    private String b;
+
+    @Autowired
+    private CustomerConfigParams customerConfigParams;
+
+    @GetMapping("/testconfig1")
+    public Map<String, String> getGlobalParams() {
+        return Map.of("param1", a, "param2", b);
+    }
+
+    @GetMapping("/testconfig2")
+    public CustomerConfigParams getGlobalParams2() {
+        return customerConfigParams;
+    }
 }
